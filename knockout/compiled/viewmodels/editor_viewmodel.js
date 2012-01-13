69bd37340ac78adb8c04b97d6cd542591078fba1
Itay Adler <itayadler@gmail.com>
2012-01-13 22:45:18 +0200

    More fixes

--- knockout/compiled/viewmodels/editor_viewmodel.js
+++ knockout/compiled/viewmodels/editor_viewmodel.js
@@ -1,7 +1,9 @@
 (function() {
   window.EditorViewModel = (function() {
     function EditorViewModel() {
-      this.aceText = ko.observable('');
+      this.aceText = ko.observable('').extend({
+        throttle: 200
+      });
     }
     return EditorViewModel;
   })();
